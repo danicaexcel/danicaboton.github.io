49974aade673c96c13831c90ec35bdb3339661da
dbauto <danicaexcel@gmail.com>
2026-09-03 14:05:01 +0800

    Route SPX demo to dedicated case study

---
+++ spx-demo-links.js
@@ -0,0 +1,4 @@
+(function(){
+  if(new URLSearchParams(location.search).get('id')!=='ocr') return;
+  ['openCase','caseLink'].forEach(id=>{const el=document.getElementById(id);if(el){el.href='spx-case-study.html?v=20260903-spx-ops1';el.target='_top';}});
+})();
