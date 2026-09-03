b1c6585c2de73a38a3e17499a55ad5b6fd6af6ef
dbauto <danicaexcel@gmail.com>
2026-09-03 14:43:12 +0800

    Point SPX demo links to refreshed case study

--- spx-demo-links.js
+++ spx-demo-links.js
@@ -1,4 +1,4 @@
 (function(){
   if(new URLSearchParams(location.search).get('id')!=='ocr') return;
-  ['openCase','caseLink'].forEach(id=>{const el=document.getElementById(id);if(el){el.href='spx-case-study.html?v=20260903-spx-ops1';el.target='_top';}});
+  ['openCase','caseLink'].forEach(id=>{const el=document.getElementById(id);if(el){el.href='spx-case-study.html?v=20260903-spx-ops2';el.target='_top';}});
 })();
