a4c1afcd3cafc9a4727bcdd48a2920e0b714a0b6
dbauto <danicaexcel@gmail.com>
2026-09-06 15:46:44 +0800

    Refresh SPX demo case-study link

--- spx-demo-links.js
+++ spx-demo-links.js
@@ -1,4 +1,4 @@
 (function(){
   if(new URLSearchParams(location.search).get('id')!=='ocr') return;
-  ['openCase','caseLink'].forEach(id=>{const el=document.getElementById(id);if(el){el.href='spx-case-study.html?v=20260903-spx-ops3';el.target='_top';}});
+  ['openCase','caseLink'].forEach(id=>{const el=document.getElementById(id);if(el){el.href='spx-case-study.html?v=20260906-spx-queue5';el.target='_top';}});
 })();
